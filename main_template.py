14a8e0a66d4a3c77e9a58338f6077d871f4e0d67
Keith Galli <k.galli603@gmail.com>
2023-10-08 15:44:01 -0400

    Initial commit

---
+++ main_template.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from flasgger import Swagger
+
+app = Flask(__name__)
+api = Api(app)
+swagger = Swagger(app)
+
+class UppercaseText(Resource):
+
+    def get(self):
+        """
+        This method responds to the GET request for this endpoint and returns the data in uppercase.
+        ---
+        tags:
+        - Text Processing
+        parameters:
+            - name: text
+              in: query
+              type: string
+              required: true
+              description: The text to be converted to uppercase
+        responses:
+            200:
+                description: A successful GET request
+                content:
+                    application/json:
+                      schema:
+                        type: object
+                        properties:
+                            text:
+                                type: string
+                                description: The text in uppercase
+        """
+        text = request.args.get('text')
+
+        return jsonify({"text": text.upper()})
+
+api.add_resource(UppercaseText, "/uppercase")
+
+if __name__ == "__main__":
+    app.run(debug=True)
